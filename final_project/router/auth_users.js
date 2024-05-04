0f8c8aa67683b4b9e68cc42c5e8151bb58403387
Naol Lemessa <lemesanaol93@gmail.com>
2024-05-04 11:58:58 +0300

    the deleting part is done

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -93,6 +93,32 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 
 }});
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.authorization["username"];
+
+  for (values in books) {
+    if (books[values].isbn== isbn) {
+    
+
+      if (values.reviews) {
+        if (values.reviews[username]) {
+          delete values.reviews[username];
+          return res.status(200).json({ message: "Review deleted successfully" });
+        } else {
+          return res.status(404).json({ message: "Review not found" });
+        }
+      } else {
+        return res.status(404).json({ message: "Review not found" });
+      }
+    }
+  }
+
+  // If the loop completes without finding the book
+  return res.status(404).json({ message: "Book not found" });
+});
+
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
